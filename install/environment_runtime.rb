71a60057fb2502315027dbb5891d107aafc11323
Dustin Fisher <dustin@dustinfisher.com>
2021-09-01 20:29:38 -0400

    start of evergreen rails application template

---
+++ install/environment_runtime.rb
@@ -0,0 +1,18 @@
+def configure_environment_runtime
+  remove_file 'config/database.yml'
+  remove_file 'config/credentials.yml.enc'
+  remove_file 'config/master.key'
+
+  append_to_file '.gitignore' do
+    <<-RUBY
+# The .env file is used for both dev and test
+# and creates more problems than it solves
+.env
+
+# .env.*.local files are where we put actual
+# secrets we need for dev and test, so
+# we really don't want this in version control
+.env.*.local”
+    RUBY
+  end
+end
---
+++ template.rb
@@ -0,0 +1,31 @@
+
+def run_template!
+  add_template_repository_to_source_path
+
+  add_gems
+
+  after_bundle do
+    configure_environment_runtime
+  end
+end
+
+# Pulled this function from https://github.com/mattbrictson/rails-template
+# and modified it for including install files
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    source_paths.unshift(tempdir = Dir.mktmpdir('evergreen-'))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      '--quiet',
+      'https://github.com/dustinfisher/evergreen.git',
+      tempdir
+    ].map(&:shellescape).join(' ')
+
+    Dir[File.join(tempdir, 'install', '*.rb')].each { |file| require file }
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+    Dir[File.join(__dir__, 'install', '*.rb')].each { |file| require file }
+  end
+end
+
+run_template!
